7f28efa0684f933ec0f1f94410e0e857ac3156ac
Mohd Qasim <qkqasimkhan02@gmail.com>
2023-04-28 10:56:51 +0530

    Add assign, add, subtract functionality

--- src/sprint.js
+++ src/sprint.js
@@ -7,4 +7,37 @@ const load = function(code) {
   }, memory);
 };
 
+const assign = function(value, cell) {
+  memory[cell] = value;
+  return memory[cell];
+};
+
+const add = function(cell1, cell2, cell3) {
+  memory[cell3] = memory[cell1] + memory[cell2];
+  return memory[cell3];
+};
+
+const sub = function(cell1, cell2, cell3) {
+  memory[cell3] = memory[cell1] - memory[cell2];
+  return memory[cell3];
+};
+
+const display = function() {
+  return JSON.stringify(memory);
+};
+
+const main = function() {
+  const code = [0, 2, 100, 0, 1, 101, 1, 100, 101, 102, 2, 100, 101, 103, 9];
+  load(code);
+  assign(2, 100);
+  assign(1, 101);
+  add(100, 101, 102);
+  sub(100, 101, 103);
+  console.log(memory);
+};
+
 exports.load = load;
+exports.assign = assign;
+exports.add = add;
+exports.sub = sub;
+exports.display = display;
--- test/sprint-test.js
+++ test/sprint-test.js
@@ -1,19 +1,43 @@
 const {describe, it} = require('node:test');
 const {strictEqual, deepStrictEqual} = require('assert');
 
-const {load} = require('../src/sprint.js');
+const {load, assign, add, sub, display} = require('../src/sprint.js');
 
 describe('sprint', function() {
-  it('should give an object contaning key as memory location', function() {
-    deepStrictEqual(load([0, 2, 100, 0, 1, 101, 9]), {
-      0: 0,
-      1: 2,
-      2: 100,
-      3: 0,
-      4: 1,
-      5: 101,
-      6: 9
+
+  describe('load', function() {
+    it('should give an object contaning key as memory location', function() {
+      deepStrictEqual(load([0, 2, 100, 0, 1, 101, 9]), {
+        0: 0,
+        1: 2,
+        2: 100,
+        3: 0,
+        4: 1,
+        5: 101,
+        6: 9
+      });
     });
   });
 
+  describe('assign', function() {
+    it('should assign value at given memory location', function() {
+      strictEqual(assign(2, 100), 2);
+    })
+
+    it('should assign value at given memory location', function() {
+      strictEqual(assign(1, 101), 1);
+    })
+  });
+
+  describe('add', function() {
+    it('should add values at given memory location', function() {
+      strictEqual(add(100, 101, 102), 3);
+    })
+  });
+
+  describe('sub', function() {
+    it('should subtract values at given memory location', function() {
+      strictEqual(sub(100, 101, 103), 1);
+    })
+  });
 });
